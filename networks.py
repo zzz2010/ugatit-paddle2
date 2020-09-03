617f7013df099b87aca8b0890bc7bf9d73418599
zhizhuo <zzz2010@gmail.com>
2020-09-03 10:55:12 -0400

    fix some bugs

--- networks.py
+++ networks.py
@@ -158,7 +158,7 @@ class adaILN(nn.Module):
     def __init__(self, num_features, eps=1e-5):
         super(adaILN, self).__init__()
         self.eps = eps
-        self.rho = Parameter((1, num_features, 1, 1),0.9)
+        self.rho = torch.Tensor(Parameter((1, num_features, 1, 1),0.9))
 
 
     def forward(self, input, gamma, beta):
@@ -167,7 +167,7 @@ class adaILN(nn.Module):
         ln_mean, ln_var = torch.mean(input, dim=[1, 2, 3], keepdim=True), torch.var(input, dim=[1, 2, 3], keepdim=True)
         out_ln = (input - ln_mean) / torch.sqrt(ln_var + self.eps)
         out = self.rho.expand(input.shape[0], -1, -1, -1) * out_in + (1-self.rho.expand(input.shape[0], -1, -1, -1)) * out_ln
-        out = out * gamma.unsqueeze(2).unsqueeze(3) + beta.unsqueeze(2).unsqueeze(3)
+        out = out * torch.Tensor(gamma).unsqueeze(2).unsqueeze(3) + torch.Tensor(beta).unsqueeze(2).unsqueeze(3)
 
         return out
 
@@ -176,9 +176,9 @@ class ILN(nn.Module):
     def __init__(self, num_features, eps=1e-5):
         super(ILN, self).__init__()
         self.eps = eps
-        self.rho = Parameter((1, num_features, 1, 1),0.0)
-        self.gamma = Parameter( (1, num_features, 1, 1),1.0)
-        self.beta = Parameter((1, num_features, 1, 1),0.0)
+        self.rho = torch.Tensor(Parameter((1, num_features, 1, 1),0.0))
+        self.gamma =torch.Tensor( Parameter( (1, num_features, 1, 1),1.0))
+        self.beta =torch.Tensor( Parameter((1, num_features, 1, 1),0.0))
 
 
     def forward(self, input):
